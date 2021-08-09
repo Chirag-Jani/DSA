9d9592c9ab0708b2662043a109be37b137c3dba3
Chirag Jani <75251761+Chirag-Jani@users.noreply.github.com>
2021-08-09 23:03:21 +0530

    Add files via upload

---
+++ 1502_arithmatic_progression.cpp
@@ -0,0 +1,62 @@
+// Q 1502. Can Make Arithmetic Progression From Sequence
+// A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+
+// Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
+
+// Example 1:
+
+// Input: arr = [3,5,1]
+// Output: true
+// Explanation: We can reorder the elements as [1,3,5] or [5,3,1] with differences 2 and -2 respectively, between each consecutive elements.
+// Example 2:
+
+// Input: arr = [1,2,4]
+// Output: false
+// Explanation: There is no way to reorder the elements to obtain an arithmetic progression.
+
+// Constraints:
+
+// 2 <= arr.length <= 1000
+// -106 <= arr[i] <= 106
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // solving
+
+    int cd = 0;
+    bool flag = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] - arr[i] == arr[j + 1] - arr[i + 1])
+            {
+                flag = true;
+            }
+        }
+    }
+    cout << flag;
+    return 0;
+}
